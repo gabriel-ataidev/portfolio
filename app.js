f3c82e21a8fbae735def2a8f4f80a0409b017f01
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-05-25 13:40:40 -0300

    tech mousemove

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 // show menu
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId);
-  
-    if (toggle && nav) {
-      toggle.addEventListener("click", () => {
-        nav.classList.toggle("show-menu");
-        console.log('working');
-      });
-    }
-  };
-  showMenu("nav-toggle", "mobile-nav");
-  
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+      console.log("working");
+    });
+  }
+};
+showMenu("nav-toggle", "mobile-nav");
 // remove menu mobile
 const navLink = document.querySelectorAll(".nav-item");
 
@@ -20,3 +19,22 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+//movement animation to happen
+const card = document.querySelector(".tech-image");
+const container = document.querySelector(".image");
+//moving animation event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+  let yAxis = (window.innerWidth / 2 - e.pageY) / 30;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//animate in
+container.addEventListener("mouseenter", (e) => {
+    card.style.transition = `all 0.1s ease-in-out`;
+});
+//animate out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  card.style.transition = `all 0.5s ease-in-out`;
+});
